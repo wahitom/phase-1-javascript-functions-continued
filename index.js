db283bf6d525a4898dad6e046ddfc87ec7c35249
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-21 22:10:38 +0300

    Completed Assignment'

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork('work from home'));
+
+function wrapAdjective(symbol = "" ){
+
+        return function encouragingPromptFunction(sentence){
+        
+            return (`You are ${symbol}${sentence}${symbol}!`) 
+        }
+    }
+    /* return function(sentence){
+        sentence = "a dedicated programmer"
+        return (`You are ${symbol}${sentence}${symbol}`)
+    } */
+
+console.log(wrapAdjective('*')('a dedicated programmer'))
+
+
